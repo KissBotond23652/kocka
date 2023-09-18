29c14fc13052a80f7a6686c4c4e8250eff0e8b2d
Szanto Benjamin <szantobenjamin97@gmail.com>
2023-09-18 19:27:44 +0200

    added missing lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
+import 'package:diece_gyak/views/diece_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: Container(),
-      ),
+      home: DieceView(),
     ),
   );
 }
